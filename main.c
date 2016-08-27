1429589c4ffc8a1725b0fbeadf697a5af0c6dc5d
GregRead <greg@gjrtechnology.co.uk>
2016-08-27 09:05:06 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+/**
+ * Description: Ubiquitous hello world example, untested.
+ * Author     : Greg 
+ * Versions
+ * 2016-08-27   Initial Version
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("Hello, world!\r\n");
+  return 0;
+}
